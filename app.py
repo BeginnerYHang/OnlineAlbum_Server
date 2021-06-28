b5dd84e152a390847066c284fbeacabfc6ba321b
android_yuanhang <1139050614@qq.com>
2021-06-28 10:42:58 +0800

    '在线相册服务端代码提交'

---
+++ app.py
@@ -0,0 +1,105 @@
+import os
+import random
+import base64
+from datetime import datetime
+
+from flask import Flask, request, json, session
+
+from com.yuanhang.model import Message, find_user_by_name, add_user, find_user, User, save_photo, find_photos_by_uid
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'whatAreYouDoing907-()297'
+
+#  定义文件上传路径
+basedir = os.path.abspath(os.path.dirname(__name__))
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'PNG', 'JPG', 'JPEG'}
+
+
+#  限制文件上传类型
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+#  对上传的文件进行重命名操作,避免文件重名
+def photo_rename():
+    now_time = datetime.now().strftime('%Y%m%d%H%M%S')
+    random_num = random.randint(10, 99)
+    return str(now_time) + str(random_num)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/register', methods=['post', 'get'])
+def register():
+    uname = request.args.get('uname')
+    password = request.args.get('password')
+    message = Message()
+    if find_user_by_name(uname) != 0:
+        message.status = 0
+        message.description = "用户名重复"
+        #json.dumps默认只支持基本类型,自定义对象转json需要先转为字典后再操作
+        return json.dumps(message.__dict__)
+    # 用户名未重复,所以可以进行注册操作
+    id = add_user(uname,password)
+    user = User(uname, password)
+    user.id = id
+    session['user'] = user.__dict__
+    return json.dumps(message.__dict__)
+
+
+@app.route('/login', methods=['post', 'get'])
+def login():
+    uname = request.args.get('uname')
+    password = request.args.get('password')
+    message = Message()
+    user = find_user(uname, password)
+    if user is None:
+        message.status = 0
+        message.description = "用户名和密码不匹配"
+    else:
+        session['user'] = {"id": user.id, "uname": uname, "password": password}
+    return json.dumps(message.__dict__)
+
+
+@app.route('/upload_photo', methods=['post'])
+def upload_photo():
+    img = request.files.get('photo')
+    filename = photo_rename()
+    message = Message()
+    #  img.filename中为上传图片名称
+    if not allowed_file(img.filename):
+        message.status = 0
+        message.description = "图片格式不支持,请重新上传"
+        return json.dumps(message.__dict__)
+    path = basedir + "/static/photo/"
+    file_path = path + filename + '.' + img.filename.rsplit('.', 1)[1]
+    # 上传成功后需要将用户id和图片
+    if session['user'] is not None:
+        save_photo(filename + '.' + img.filename.rsplit('.', 1)[1], session['user']['id'])
+        img.save(file_path)
+    return json.dumps(message.__dict__)
+
+#  根据session中的用户数据查询photo中的路径名称
+@app.route('/download_photo', methods=['get','post'])
+def download_photo():
+    if session['user'] is not None:
+        id = session['user']['id']
+        photos = find_photos_by_uid(id)
+        # 将图片转为Base64编码格式
+        list = []
+        print(photos)
+        for photo in photos:
+            with open(r'{}/static/photo/{}'.format(basedir,photo.name),'rb') as f:
+                print(f.name)
+                list.append(base64.b64encode(f.read()).decode())
+        print(list)
+        return json.dumps(list)
+
+
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ com/yuanhang/model.py
@@ -0,0 +1,93 @@
+﻿# 创建对象的基类
+from enum import Enum
+
+from sqlalchemy import Column, INT, String, create_engine, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+
+Base = declarative_base()
+
+engine = create_engine('mysql+pymysql://root:123456@127.0.0.1:3306/online_album')
+DBSession = sessionmaker(bind=engine)
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(INT, primary_key=True, nullable=False, autoincrement=True)
+    uname = Column(String(32), nullable=False)
+    password = Column(String(256))
+
+    #  一的一方写relationship,可以通过表名.字段得到一个列表
+    #  多的一方写ForeignKey,可以通过表名.(一的一方的backref)对一的一方进行读取和修改'''
+    photos = relationship('Photo')
+
+    def __init__(self, uname, password):
+        self.uname = uname
+        self.password = password
+
+
+class Photo(Base):
+    __tablename__ = 'photo'
+    __table_args__ = {'mysql_engine': 'InnoDB'}
+    id = Column(INT, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(256), nullable=False)
+    uid = Column(INT, ForeignKey('user.id'))
+
+    def __init__(self, name, uid):
+        self.name = name
+        self.uid = uid
+
+
+#  添加用户
+def add_user(uname, password):
+    session = DBSession()
+    user = User(uname, password)
+    session.add(user)
+    session.flush()
+    # flush后可以获得插入后的uid
+    id = user.id
+    session.commit()
+    session.close()
+    return id
+
+
+def find_user_by_name(uname):
+    session = DBSession()
+    count = session.query(User).filter_by(uname=uname).count()
+    session.close()
+    return count
+
+
+# 判断用户是否登录成功
+def find_user(uname, password):
+    session = DBSession()
+    user = session.query(User).filter_by(uname=uname, password=password).first()
+    session.close()
+    return user
+
+def save_photo(photo_name, uid):
+    photo = Photo(photo_name, uid)
+    session = DBSession()
+    session.add(photo)
+    session.commit()
+    session.close()
+
+
+def find_photos_by_uid(uid):
+    session = DBSession()
+    photos = session.query(Photo).filter_by(uid=uid).all()
+    if photos is None:
+        return []
+    return photos
+
+
+
+class Message(object):
+
+    #  描述请求信息的类,status为"ok/error",ok则携带数据data,error则传入错误描述description
+    #  python支持实例自定义属性
+    def __init__(self, status=1):
+        self.status = status
+
+if __name__ == '__main__':
+    add_user("aaa","bbb")
